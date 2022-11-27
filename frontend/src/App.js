cfdae3a94bfd6c6e84716cd476a66fe1ab92b7a2
Bruce Tushabe <brucetushabe@gmail.com>
2022-11-27 19:39:14 +0300

    Yoo , new changes ..more

---
+++ frontend/src/App.js
@@ -0,0 +1,24 @@
+
+import './App.css';
+import Navbar from './components/Navbar';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+
+function App() {
+  return (
+  <>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path='/' exact />
+
+      </Routes>
+
+    </Router>
+    
+     
+  </>
+  
+  );
+}
+
+export default App;
---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,24 @@
+import React, {useState} from 'react';
+
+import {Link} from 'react-router-dom';
+
+function Navbar(){
+
+    return (
+        <>
+
+        <nav className="navbar">
+            <div className="navbar-container">
+                <Link to="/" className= "navbar-logo">
+                    RAPP
+                </Link>
+            </div>
+        
+        </nav>
+
+        </>
+    )
+
+}
+
+export default Navbar;
