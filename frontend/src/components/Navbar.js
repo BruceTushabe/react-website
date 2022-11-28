f60c2b8251f1f77b5c61d4f211c0d6061ce7bbc0
Bruce Tushabe <brucetushabe@gmail.com>
2022-11-28 12:26:21 +0300

    Let's build on

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -4,14 +4,22 @@ import {Link} from 'react-router-dom';
 
 function Navbar(){
 
+    const [click, setClick] = useState(false);
+
+    const handleClick = () => setClick(!click);
+
     return (
         <>
 
         <nav className="navbar">
             <div className="navbar-container">
                 <Link to="/" className= "navbar-logo">
-                    RAPP
+                    RAPP <i className='fab fa-typo3'/>
                 </Link>
+
+                <div className='menu-icon' onClick = {handleClick}>
+                    <i className={click? 'fas fa-times' : 'fas fa-bars'} />
+                </div>
             </div>
         
         </nav>
