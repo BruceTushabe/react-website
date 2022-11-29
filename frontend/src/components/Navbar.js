650f8bf260b67c304b16ed8a857a9a424a2b3dfe
Bruce Tushabe <brucetushabe@gmail.com>
2022-11-29 23:22:53 +0300

    all updates

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -8,6 +8,8 @@ function Navbar(){
 
     const handleClick = () => setClick(!click);
 
+    const closeMobileMenu = () => setClick(false);
+
     return (
         <>
 
@@ -20,8 +22,30 @@ function Navbar(){
                 <div className='menu-icon' onClick = {handleClick}>
                     <i className={click? 'fas fa-times' : 'fas fa-bars'} />
                 </div>
+                <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+                    <li className='nav-item'>
+                        <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+                            Home
+                        </Link>
+                    </li>
+                    <li className='nav-item'>
+                        <Link to='/services' className='nav-links' onClick={closeMobileMenu}>
+                            Services
+                        </Link>
+                    </li>
+                    <li className='nav-item'>
+                        <Link to='/products' className='nav-links' onClick={closeMobileMenu}>
+                            Products
+                        </Link>
+                    </li>
+                    <li className='nav-item'>
+                        <Link to='/' className='nav-links-mobile' onClick={closeMobileMenu}>
+                            Sign Up
+                        </Link>
+                    </li>
+                </ul>
             </div>
-        
+
         </nav>
 
         </>
